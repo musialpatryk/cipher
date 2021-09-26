7713944f88e28833de2f6a4f34f7ba43e3d46732
Patryk <musial.patryk10@gmail.com>
2021-09-26 14:18:09 +0200

    Adjust init structure

--- src/main.component.ts
+++ src/main.component.ts
@@ -2,9 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './main.component.html'
 })
-export class AppComponent {
+export class MainComponent {
   title = 'cipher';
 }
--- src/main.module.ts
+++ src/main.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { MainComponent } from './main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    MainComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [MainComponent]
 })
-export class AppModule { }
+export class MainModule { }
