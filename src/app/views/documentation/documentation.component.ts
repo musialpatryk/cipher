984ec0bf6c3efd2c9e160695c5a223d66f41affe
musialpatryk <musial.patryk10@gmail.com>
2021-10-31 17:41:42 +0100

    Add content to documentation

--- src/app/views/documentation/documentation.component.ts
+++ src/app/views/documentation/documentation.component.ts
@@ -1,9 +1,30 @@
 import {Component} from '@angular/core';
+import {ICurrentVisualizationStep} from '../../components/visualization/visualization.component';
 
 
 @Component({
   selector: 'app-documentation',
-  templateUrl: './documentation.component.html'
+  templateUrl: './documentation.component.html',
+  styleUrls: ['./documentation.component.css']
 })
 export class DocumentationComponent {
+  exampleStep: ICurrentVisualizationStep = {
+    step: {
+      id: 0,
+      prevChar: 'S',
+      nextChar: 'W',
+      keyChar: 'E'
+    },
+    index: 0
+  };
+
+  decodeExampleStep: ICurrentVisualizationStep = {
+    step: {
+      id: 0,
+      prevChar: 'S',
+      nextChar: 'W',
+      keyChar: 'E'
+    },
+    index: 0
+  };
 }
